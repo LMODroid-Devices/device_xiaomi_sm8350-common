38bfffe3fddcce60ae7c4df380767540f753c936
Erfan Abdi <erfangplus@gmail.com>
2025-09-21 22:32:47 +0330

    Fix lineage_powershare
    Change-Id: I6877c1eb0dd93ef726bcf5aabd7c27bd2b638570

--- common.mk
+++ common.mk
@@ -323,7 +323,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # PowerShare
-$(call soong_config_set,lmodroid_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
+$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 
 # QTI
 PRODUCT_COPY_FILES += \
